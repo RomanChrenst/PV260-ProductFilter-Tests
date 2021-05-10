da8ab3a087d9686897016b1e7103f3f1714e98a2
Roman Chrenšť <xchrenst@fi.muni.cz>
2021-05-10 13:14:28 +0200

    Added testing dependancies and created test files

---
+++ src/test/java/cz/muni/fi/pv260/productfilter/AtLeastNOfFilterTest.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pv260.productfilter;
+
+
+public class AtLeastNOfFilterTest {
+
+    /*
+    –The constructor throws the exceptions as documented
+    –The filter passes if at least exactly n child filters pass
+    –The filter fails if at most n-1 child filters pass
+    */
+
+}
---
+++ src/test/java/cz/muni/fi/pv260/productfilter/ControllerTest.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pv260.productfilter;
+
+
+public class ControllerTest {
+
+    /*
+    –The controller sends exactly the products selected by the provided filter to Output
+    –The controller logs the message in documented format on success
+    –If exception occurs when obtaining the Product data, Controller logs this exception
+    –If exception occurs when obtaining the Product data, nothing is passed to the Output
+    */
+
+}
