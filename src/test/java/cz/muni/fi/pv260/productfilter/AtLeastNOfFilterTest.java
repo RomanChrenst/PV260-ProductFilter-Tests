903feea85e725aa651bcf277e759bb5348eb6fcd
Roman Chrenšť <xchrenst@fi.muni.cz>
2021-05-10 15:14:35 +0200

    Added tests for the AtLeastNOfFilter class

--- src/test/java/cz/muni/fi/pv260/productfilter/AtLeastNOfFilterTest.java
+++ src/test/java/cz/muni/fi/pv260/productfilter/AtLeastNOfFilterTest.java
@@ -1,12 +1,43 @@
 package cz.muni.fi.pv260.productfilter;
 
+import static org.mockito.Mockito.*;
+import org.junit.Test;
 
 public class AtLeastNOfFilterTest {
 
-    /*
-    –The constructor throws the exceptions as documented
-    –The filter passes if at least exactly n child filters pass
-    –The filter fails if at most n-1 child filters pass
-    */
+    @Test(expected = FilterNeverSucceeds.class)
+    public void testConstructorThrowsFilterNeverSucceeds() {
+        AtLeastNOfFilter filter1 = mock(AtLeastNOfFilter.class);
+        AtLeastNOfFilter filter2 = mock(AtLeastNOfFilter.class);
+        new AtLeastNOfFilter<AtLeastNOfFilter>(3, filter1, filter2);
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorThrowsIllegalArgumentException() {
+        AtLeastNOfFilter filter1 = mock(AtLeastNOfFilter.class);
+        AtLeastNOfFilter filter2 = mock(AtLeastNOfFilter.class);
+        new AtLeastNOfFilter<AtLeastNOfFilter>(0, filter1, filter2);
+    }
+
+    @Test
+    public void testFilterPassesIfNChildFiltersPass() {
+        AtLeastNOfFilter arg_filter = mock(AtLeastNOfFilter.class);
+        AtLeastNOfFilter filter1 = mock(AtLeastNOfFilter.class);
+        AtLeastNOfFilter filter2 = mock(AtLeastNOfFilter.class);
+        when(filter1.passes(arg_filter)).thenReturn(true);
+        when(filter2.passes(arg_filter)).thenReturn(true);
+        AtLeastNOfFilter main_filter = new AtLeastNOfFilter(2, filter1, filter2);
+        assert(main_filter.passes(arg_filter));
+    }
+
+    @Test
+    public void testFilterFailsIfLeesThanNChildFiltersPass() {
+        AtLeastNOfFilter arg_filter = mock(AtLeastNOfFilter.class);
+        AtLeastNOfFilter filter1 = mock(AtLeastNOfFilter.class);
+        AtLeastNOfFilter filter2 = mock(AtLeastNOfFilter.class);
+        when(filter1.passes(arg_filter)).thenReturn(false);
+        when(filter2.passes(arg_filter)).thenReturn(true);
+        AtLeastNOfFilter main_filter = new AtLeastNOfFilter(2, filter1, filter2);
+        assert(!main_filter.passes(arg_filter));
+    }
 }
